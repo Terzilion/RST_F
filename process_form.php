eae030e3cac5d3f5c4151a505e4a7a10a0c89324
TERZ <87185082+Terzilion@users.noreply.github.com>
2023-11-14 20:39:14 +0700

    Add files via upload

---
+++ process_form.php
@@ -0,0 +1,59 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+
+require __DIR__ . '/phpmailer/src/PHPMailer.php';
+require __DIR__ . '/phpmailer/src/SMTP.php';
+require __DIR__ . '/phpmailer/src/Exception.php';
+
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+if (isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $message = $_POST["message"];
+    $subject = $_POST["subject"];
+    
+    try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'rstsupr@gmail.com';                     //SMTP username
+        $mail->Password   = 'jpsg oupq spge lydy';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+        //Recipients
+        $mail->setFrom('rstsupr@gmail.com', 'Mailer');
+        $mail->addAddress($email, $name);     //Add a recipient
+    
+        //Attachments
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Мы получили ваш запрос';
+        $mail->Body    = 'Спасибо, $name, за отправку формы. Мы получили ваш запрос.\n\nИмя: $name\nEmail: $email\nНомер телефона: $phone\nСообщение: $message';
+    
+        $mail->send();
+
+        $to_owner = "rstsupr@gmail.com";
+        $subject_owner = "Новая форма отправлена";
+        $message_owner = "Новая форма была отправлена.\n\nИмя: $name\nEmail: $email\nНомер телефона: $phone\nСообщение: $message";
+
+        mail($to_owner, $subject_owner, $message_owner);
+
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
